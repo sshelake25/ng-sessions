14d07e32815ef0d376df2b9557c3509bd026d335
precodepath <precodepath@gmail.com>
2021-03-26 08:35:48 +0530

    reactive form validation

--- src/app/reactive-form-learning/reactive-form-learning.component.ts
+++ src/app/reactive-form-learning/reactive-form-learning.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-learning',
@@ -23,9 +23,16 @@ export class ReactiveFormLearningComponent implements OnInit {
   ngOnInit(): void {
 
     this.loginForm = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl(),
-      langauge : new FormControl()
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.max(5)
+      ]),
+      langauge : new FormControl('English')
     });
 
   }
